fef0a682119b832ab28dd4ba40458bf3ab35727f
bhavanireddy20 <100672662+bhavanireddy20@users.noreply.github.com>
2025-12-27 22:30:23 +0530

    Update app.java

--- app.java
+++ app.java
@@ -5,3 +5,5 @@ past is past just focus on present what you need to futhur to get successful
   this year you;ll succeded in life
   i know 
 i am proud og you misss bhavani
+try with a hope, dont lose hope
+  everything will come at right moment
