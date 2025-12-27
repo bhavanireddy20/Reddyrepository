a0a23ee5eaa06c6ed334bb8ce7c6957aca23885f
bhavanireddy20 <100672662+bhavanireddy20@users.noreply.github.com>
2025-12-27 23:01:28 +0530

    Update app.java

--- app.java
+++ app.java
@@ -7,6 +7,6 @@ past is past just focus on present what you need to futhur to get successful
 i am proud og you misss bhavani
 try with a hope, dont lose hope
   everything will come at right moment
-  he is not interested with you anymore, dont wait for him
+  he is not interested with you anymore, dont wait for him, he just conusmed you thst it
   dont pour too much water on dead plant
   just love yourself as much as you can
