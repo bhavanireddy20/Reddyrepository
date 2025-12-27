d12074e7dcacf16383f5a13757b36ab88544388f
bhavanireddy20 <100672662+bhavanireddy20@users.noreply.github.com>
2025-12-27 22:50:49 +0530

    Update app.java

--- app.java
+++ app.java
@@ -8,5 +8,4 @@ i am proud og you misss bhavani
 try with a hope, dont lose hope
   everything will come at right moment
   he is not interested with you anymore 
-just forget him asap
   just love yourself as much as you can
